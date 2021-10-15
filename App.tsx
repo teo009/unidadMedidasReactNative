39107d7f8e1c73ffafa3bfb6a1df2d7bdabc3d74
teo09 <teo.obando09@gmail.com>
2021-10-15 11:38:40 -0600

    Validación de centimetros(cm) a metros (mt)

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Converter from './src/components/Converter';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Converter />
     </View>
   );
 }
@@ -14,8 +15,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#87cefa',
     alignItems: 'center',
     justifyContent: 'center',
+    fontSize: 10,
   },
 });
---
+++ src/components/Converter.tsx
@@ -0,0 +1,113 @@
+import React, { useState } from 'react'
+import { View, Text, TextInput, StyleSheet, Button, } from 'react-native';
+
+
+//milimetros, centimetros, metros, kilometros
+
+function Converter() {
+
+    //Variables de estado
+    const [origen, setOrigen] = useState('cm')
+    const [destino, setDestino] = useState('mt')
+    const [convertir, setConvertir] = useState(0)
+    const [resultado, setResultado] = useState(0)
+
+    const handleChangeOrigen = (texto: string) => {
+        setOrigen(texto)
+    }
+    const handleChangeDestino = (texto: string) => {
+        setDestino(texto)
+    }
+    const handleChangeConvertir = (texto: string) => {
+        //Convertir el string a numero, porque los inputs
+        //siempre devuelven strings
+        const number = parseFloat(texto)
+        setConvertir(number)
+    }
+    const handleCalcular = () => {
+        //alert('holaaa')
+        if (origen === 'cm' && destino === 'mt') {
+            const result = convertir / 100
+            //console.log(result)
+            setResultado(result)
+        }
+    }
+
+    return (
+        <View>
+
+            <Text
+                style={{
+                    fontSize: 25,
+                    paddingVertical: 60,
+                }}
+            >
+                Convertidor Mamalon :u
+            </Text>
+
+            <Text>Origen:</Text>
+            <TextInput 
+                style={styles.input}
+                defaultValue={origen}
+                maxLength={2}
+                onChangeText={(text) => handleChangeOrigen(text)}
+            />
+
+            <Text>Destino:</Text>
+            <TextInput 
+                style={styles.input}
+                defaultValue={destino}
+                maxLength={2}
+                onChangeText={(text) => handleChangeDestino(text)}
+            />
+
+            <Text>Convertir:</Text>
+            <TextInput 
+                style={styles.input}
+                defaultValue={convertir.toString()}
+                maxLength={9}
+                onChangeText={
+                    (text) => handleChangeConvertir(text)
+                }
+            />
+
+            <Text>Resultado:</Text>
+            <TextInput 
+                style={styles.inputError}
+                defaultValue={resultado.toString()}
+            />
+
+            <View style={styles.button}>
+            <Button 
+                title="Calcular"
+                onPress={handleCalcular}           
+            />
+            </View>
+
+        </View>
+    )
+
+}
+
+export default Converter
+
+const styles = StyleSheet.create({
+    input: {
+        borderColor: 'white',
+        borderWidth: 3,
+        borderRadius: 10,
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        textAlign: 'center',
+    },
+    inputError: {
+        borderColor: 'red',
+        borderWidth: 3,
+        borderRadius: 10,
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+    },
+    button: {
+        paddingVertical: 40,
+    }
+})
